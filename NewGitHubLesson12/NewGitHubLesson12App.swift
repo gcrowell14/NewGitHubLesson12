3a5b47c70e1e81489b9ed123af81000bd077722d
Grace Crowell <gmcrow20@gmail.com>
2023-04-22 12:11:12 -0400

    Initial Commit

---
+++ NewGitHubLesson12/NewGitHubLesson12App.swift
@@ -0,0 +1,17 @@
+//
+//  NewGitHubLesson12App.swift
+//  NewGitHubLesson12
+//
+//  Created by Grace Crowell on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct NewGitHubLesson12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
